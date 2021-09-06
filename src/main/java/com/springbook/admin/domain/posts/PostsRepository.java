f6ef183ac18f616b934d0d68f563d23d4b26d43e
philos1234 <48795102+philos1234@users.noreply.github.com>
2021-09-06 21:22:04 +0900

    글 등록

--- src/main/java/com/springbook/admin/domain/posts/PostsRepository.java
+++ src/main/java/com/springbook/admin/domain/posts/PostsRepository.java
@@ -1,8 +1,16 @@
 package com.springbook.admin.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //주의 Entity 클래스와 기본 Entity Repository가 함께 위치해야 함
 public interface PostsRepository extends JpaRepository<Posts,Long> {
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
+    //실제로는 조회용 프레임워크를 사용함  querydsl , jooq, MyBatis etc...
+    // < Querydsl>
 }
--- src/main/java/com/springbook/admin/service/posts/PostsService.java
+++ src/main/java/com/springbook/admin/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.springbook.admin.service.posts;
 
 import com.springbook.admin.domain.posts.Posts;
 import com.springbook.admin.domain.posts.PostsRepository;
+import com.springbook.admin.web.dto.PostsListResponseDto;
 import com.springbook.admin.web.dto.PostsResponseDto;
 import com.springbook.admin.web.dto.PostsSaveRequestDto;
 import com.springbook.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,6 +36,15 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+
+    }
+
+
+    @Transactional(readOnly=true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
 
     }
 }
