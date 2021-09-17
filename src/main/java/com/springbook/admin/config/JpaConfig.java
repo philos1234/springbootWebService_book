ee7276d404a48f1358855d29e3636e6f760f9f8d
philos1234 <48795102+philos1234@users.noreply.github.com>
2021-09-17 23:57:45 +0900

    test config

---
+++ src/main/java/com/springbook/admin/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.springbook.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // JPA Audition 활성화
+public class JpaConfig {
+
+}
