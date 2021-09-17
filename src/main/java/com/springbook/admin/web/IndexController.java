fc6dc0c71c632f5a3551222bd7ea06fb9ed084b8
philos1234 <48795102+philos1234@users.noreply.github.com>
2021-09-17 22:03:29 +0900

    @LoginUser

--- src/main/java/com/springbook/admin/web/IndexController.java
+++ src/main/java/com/springbook/admin/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springbook.admin.web;
 
+import com.springbook.admin.config.auth.LoginUser;
 import com.springbook.admin.config.auth.dto.SessionUser;
 import com.springbook.admin.service.posts.PostsService;
 
@@ -20,10 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
